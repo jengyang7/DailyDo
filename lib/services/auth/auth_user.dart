18e8d1140487b7e8c9cbf57caad29241a3211279
jengyang07 <jengyang1307@gmail.com>
2023-05-03 17:39:35 +0800

    step-10 Unit Testing for AuthService

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 @immutable
 class AuthUser {
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified});
 
   // get a copy from Firebase User
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user) => AuthUser(
+        isEmailVerified: user.emailVerified,
+      );
 }
