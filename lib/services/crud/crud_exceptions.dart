3c875320c5309bcbef2a88c9c58e3be5db523321
jengyang07 <jengyang1307@gmail.com>
2023-05-04 01:03:16 +0800

    step-11 CRUD Local Storage

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,15 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnabletoGetDocumentDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class CouldNoteDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotFindNote implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
